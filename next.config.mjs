78fee21431aee63349657f0ee5669fd00dea8a31
hofeys1-cmd <hofeys1@gmail.com>
2025-08-26 13:13:22 +0330

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+import withPWA from 'next-pwa'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+})(nextConfig)
